348aa6bad3d24fe3287ef02733d6ccca046f2040
zakythr <37214711+zakythr@users.noreply.github.com>
2019-02-22 15:35:00 +0700

    Add files via upload

---
+++ nomer1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+unzip /home/zaky/Downloads/nature.zip
+File=/home/zaky/Downloads/nature/*
+mkdir /home/zaky/Documents/naturejpg
+tujuan=/home/zaky/Documents/naturejpg
+i=0
+for f in $File
+do
+	chmod 777 $f
+	echo "ubah permission"
+	base64 -d $f | xxd -r > $tujuan/$i.jpg
+	let i=$i+1
+done
